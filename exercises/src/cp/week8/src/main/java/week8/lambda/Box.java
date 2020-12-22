b7e9843986d56874597d159f10dc0c3c8028cfbe
Eros Fabrici <eros.fabrici@gmail.com>
2020-12-22 21:08:17 +0100

    exercise 5 done

--- exercises/src/cp/week8/src/main/java/week8/lambda/Box.java
+++ exercises/src/cp/week8/src/main/java/week8/lambda/Box.java
@@ -1,5 +1,9 @@
 package week8.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Box<T> {
     private T content;
 
@@ -15,5 +19,9 @@ public class Box<T> {
 
     public <O> O apply(BoxFunction<T, O> boxFunction) {
         return boxFunction.apply(content);
+    }
+
+    public static <I, O> List<Box<O>> applyToAll(List<Box<I>> list, BoxFunction<I, O> boxFunction) {
+        return list.stream().map(iBox -> new Box<O>(iBox.apply(boxFunction))).collect(Collectors.toList());
     }
 }
